744c56d13fbd50a52f386d65d1e9c507750161ba
Christopher Fuller <christopher.fuller@gotinder.com>
2024-01-07 13:29:16 -0800

    Format attributes on their own lines

--- Sources/CombineUI/UIKit/Bindings/Bindable+UITextField.swift
+++ Sources/CombineUI/UIKit/Bindings/Bindable+UITextField.swift
@@ -7,11 +7,15 @@ import UIKit
 
 extension Bindable where Target: UITextField {
 
-    @preconcurrency @MainActor public var text: Binding<String> {
+    @preconcurrency
+    @MainActor
+    public var text: Binding<String> {
         Binding(self, for: \.text)
     }
 
-    @preconcurrency @MainActor public var attributedText: Binding<AttributedString> {
+    @preconcurrency
+    @MainActor
+    public var attributedText: Binding<AttributedString> {
         Binding(self) { $0.attributedText = NSAttributedString($1) }
     }
 }
