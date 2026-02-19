00ed031d55b4bf48c10e45d878b806f7450d226f
Christopher Fuller <christopher.fuller@gotinder.com>
2026-02-19 14:11:26 -0800

    Apply standard Swift package configuration (#35)
    * Apply standard Swift package configuration
    
    * Treat warnings as errors in release
    
    * Do not set configuration in test step
    
    * Do not set configuration in build step
    
    * Apply standard Swift package configuration
    
    * Add missing comma
    
    * Assign separately to avoid build issue

--- Package.swift
+++ Package.swift
@@ -1,6 +1,12 @@
-// swift-tools-version:5.8
+// swift-tools-version:5.10
 
+import Foundation
 import PackageDescription
+
+let environment = ProcessInfo.processInfo.environment
+
+let treatWarningsAsErrors = environment["CI"] == "true"
+let enableSwiftLintBuildToolPlugin = environment["CODEQL_DIST"] == nil
 
 let package = Package(
     name: "CombineUI",
@@ -16,10 +22,10 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/realm/SwiftLint.git",
-            exact: "0.56.2"),
+            exact: "0.59.1"),
         .package(
             url: "https://github.com/Quick/Nimble.git",
-            exact: "13.4.0"),
+            exact: "14.0.0"),
     ],
     targets: [
         .target(
@@ -30,16 +36,26 @@ let package = Package(
                 "CombineUI",
                 "Nimble",
             ]),
-    ]
+    ],
 )
 
 package.targets.forEach { target in
 
-    target.swiftSettings = [
-        .enableExperimentalFeature("StrictConcurrency"),
-    ]
+    // TODO: Remove upon enabling Swift 6 language mode:
+    target.swiftSettings = (target.swiftSettings ?? []) + [.enableExperimentalFeature("StrictConcurrency")]
 
-    target.plugins = [
-        .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint"),
-    ]
+    if treatWarningsAsErrors {
+        target.swiftSettings = (target.swiftSettings ?? []) + [
+            // TODO: Remove unsafe flag upon upgrading to Swift tools v6.2 and uncomment subsequent settings:
+            .unsafeFlags(["-warnings-as-errors"]),
+//            .treatAllWarnings(as: .error),
+//            .treatWarning("DeprecatedDeclaration", as: .warning),
+        ]
+    }
+
+    if enableSwiftLintBuildToolPlugin {
+        target.plugins = (target.plugins ?? []) + [
+            .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint"),
+        ]
+    }
 }
